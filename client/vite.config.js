27b1b37dfdfb52325555883a18a403407b31b5cd
erilar-ir <56081071+erilar-ir@users.noreply.github.com>
2021-09-14 19:57:56 +0300

    Update envs

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import config from "config";
 
+let baseUrl
+if (process.env.NODE_ENV === 'production') {
+  baseUrl = process.env.BASE_URL
+}
+if (process.env.NODE_ENV === 'development') {
+  baseUrl = config.get('baseUrl')
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.BASE_URL,
+        target: baseUrl,
         changeOrigin: true
       }
     }

9d54221f9c0e8ad5b128f756ba0929a429b95e7b
erilar-ir <56081071+erilar-ir@users.noreply.github.com>
2021-10-13 17:16:11 +0300

    error middleware tweak

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -1,10 +1,8 @@
 const ErrorHandler = require('../exceptions/error-handler')
 
 module.exports = function (error, request, response, next) {
-    console.log(error)
     if (error instanceof ErrorHandler) {
         return response.status(error.status).json({message: error.message, errors: error.errors})
     }
-
     return response.status(500).json({message: `Uncaught error. Error message is: ${error.message}`})
 }
